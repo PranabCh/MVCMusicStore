d638ba496a4fbe4b31f63cfc2cc1aba48aa1f7c2
PranabCh <juranedu@gmail.com>
2016-04-19 12:18:17 +0600

    Add project files.

---
+++ MVCMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Browse()
+        {
+            return "Hello form store.Browse()";
+        }
+
+    }
+}
